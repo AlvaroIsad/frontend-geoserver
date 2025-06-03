6b0523d69584236f0b99d427c0ddf261c75c69d7
AlvaroIsad <xddxtroll@gmail.com>
2025-06-03 18:22:58 -0500

    Update maps.js

--- js/maps.js
+++ js/maps.js
@@ -4,10 +4,26 @@ var map = L.map("map", {
     zoomControl: false // Oculta el control de zoom
 });
 
+function reloadVisibleWMSLayers() {
+    map.eachLayer(function(layer) {
+        if (layer instanceof L.TileLayer.WMS && map.hasLayer(layer)) {
+            layer.setParams({ _random: Math.random() }); // fuerza recarga
+        }
+    });
+}
+
+// Crear capas base con lógica para recargar WMS cuando se activan
 var osy = L.tileLayer("https://mt1.google.com/vt/lyrs=y&x={x}&y={y}&z={z}");
-var osm = L.tileLayer("https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}"); 
+osy.on('add', reloadVisibleWMSLayers);
+
+var osm = L.tileLayer("https://mt1.google.com/vt/lyrs=m&x={x}&y={y}&z={z}");
+osm.on('add', reloadVisibleWMSLayers);
+
 var oss = L.tileLayer("https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}").addTo(map);
+oss.on('add', reloadVisibleWMSLayers);
+
 var osz = L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png");
+osz.on('add', reloadVisibleWMSLayers);
 
 
 var usuarios = L.tileLayer.wms("https://91f6-2803-a3e0-1956-30c0-a9d7-83fa-8096-28ae.ngrok-free.app/geoserver/catastro_huaraz/wms?",{
@@ -108,19 +124,6 @@ var wms = {
                 "Sectores":sector
 
 };
-
-// ⬇️ ⬇️ AQUI VA el código para recargar capas WMS al cambiar de base ⬇️ ⬇️
-function reloadVisibleWMSLayers() {
-    map.eachLayer(function(layer) {
-        if (layer instanceof L.TileLayer.WMS && map.hasLayer(layer)) {
-            layer.setParams({ _random: Math.random() });
-        }
-    });
-}
-
-map.on("baselayerchange", function () {
-    reloadVisibleWMSLayers();
-});
 
 var ubi = {
                 "Huaraz": L.layerGroup().on("add", function () {
